7493edd6b83f7680dfdb070c6a4daaf9c3facb1f
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2019-06-07 16:15:48 +0200

    Aération

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,16 +1,5 @@
-
-
-
-
-// Signature aléatoire
-$(document).ready(function() {
-    var quotes = new Array("<p>Made in France with ❤️<br> by <a href='https://victor-azevedo.me/'>Victor Azevedo</a> & <a href='https://tahoe.be'>Tahoe Beetschen</a></p>", "<p>Made in France with ❤️<br> by <a href='https://tahoe.be'>Tahoe Beetschen</a> & <a href='https://victor-azevedo.me/'>Victor Azevedo</a></p>");
-    var randno = Math.floor(Math.random()*(quotes.length));
-    $('.signature').append(quotes[randno]);
-    console.log(randno);
-});
-
-
-
-
-
+if (Math.random() > .5) {
+	$('.signature').append("<p>Made in France with ❤️<br> by <a href='https://victor-azevedo.me/'>Victor Azevedo</a> & <a href='https://tahoe.be'>Tahoe Beetschen</a></p>");
+} else {
+	$('.signature').append("<p>Made in France with ❤️<br> by <a href='https://tahoe.be'>Tahoe Beetschen</a> & <a href='https://victor-azevedo.me/'>Victor Azevedo</a></p>");
+}
