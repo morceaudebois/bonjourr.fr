7bfbac886159b0e19164a98feab1fc743f59ae7b
Tahoooe <50768582+Tahoooe@users.noreply.github.com>
2019-06-28 18:12:15 +0200

    Big update
    - Credit section
    - About section
    - Video doesn't start loading until the rest of the page is finished loading
    - Better SEO
    - 404 page
    - French version
    - Retina & standard ready

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     var quotes = new Array("<p>Made in France with ❤️<br> by <a href='https://victor-azevedo.me/'>Victor Azevedo</a> & <a href='https://tahoe.be'>Tahoe Beetschen</a></p>", "<p>Made in France with ❤️<br> by <a href='https://tahoe.be'>Tahoe Beetschen</a> & <a href='https://victor-azevedo.me/'>Victor Azevedo</a></p>");
     var randno = Math.floor(Math.random()*(quotes.length));
     $('.signature').append(quotes[randno]);
-    console.log(randno);
 });
 
 
@@ -16,4 +15,33 @@ grecaptcha.ready(function() {
 
 });
 
+window.onload = function() {
+	 document.getElementById('insert_video').innerHTML = '<video id="screen" autoplay="autoplay" loop="loop" muted="muted"><source type="video/mp4" src="src/images/notagif.mp4"></video>';
+}
 
+function CheckMobile() {
+	var Mobile = true;
+
+	if( /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+function AllowOnline() {
+	var isChecked = CheckMobile();
+
+	if (isChecked === false) {
+		window.open("https://online.bonjourr.fr"); 
+  		
+	}
+	else {
+  		alert("Bonjourr is not available on mobile devices. You can use it on your computer.");
+	}
+}
+
+// if (checkMobile()) alert("Pas dispo sur mobile");
+// ou 
+// (checkMobile() ? alert("Pas dispo sur mobile") : window.open(url))
