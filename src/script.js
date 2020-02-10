643f424c118b4529262b89f130f61a28177d385c
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-02-10 12:51:25 +0100

    Fixed browser check

--- src/script.js
+++ src/script.js
@@ -1,4 +1,3 @@
-
 window.onload = function() {
 
 	//signature aleatoire
@@ -8,31 +7,36 @@ window.onload = function() {
 		concat = (Math.random() > .5 ? `${victor} & ${tahoe}` : `${tahoe} & ${victor}`);
 
 	signature.innerHTML = "<p>Made in France with ❤️<br> by " + concat;
-
-
-
+	
 
 	//download for different browsers
 	const dl_a = document.getElementById("mainDownload"),
 		dl_img = document.getElementById("dl_icon"),
 		dl_desc = document.getElementById("dl_desc");
 
-	if (navigator.userAgent.indexOf("Firefox") !== -1) {
-		dl_a.href = "https://addons.mozilla.org/en-US/firefox/addon/bonjourr-startpage/";
-		dl_desc.innerText = "Get Bonjourr on Firefox";
-		dl_img.src = "src/images/firefox.svg";
-	}
-	else if (navigator.userAgent.indexOf("OPR") !== -1) {
+	const isMobile = (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? true : false);
+
+	const isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0,
+		isFirefox = typeof InstallTrigger !== 'undefined',
+		isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification)),
+		isChrome = !!window.chrome;
+
+	if (isMobile || isOpera) {
 		dl_a.href = "https://online.bonjourr.fr";
 		dl_desc.innerText = "Try Bonjourr online";
 		dl_img.style.display = "none";
 	}
-	else if (navigator.userAgent.indexOf("Safari") !== -1) {
+	else if (isFirefox) {
+		dl_a.href = "https://addons.mozilla.org/en-US/firefox/addon/bonjourr-startpage/";
+		dl_desc.innerText = "Get Bonjourr on Firefox";
+		dl_img.src = "src/images/firefox.svg";
+	}
+	else if (isSafari) {
 		dl_a.href = "#otherdl";
 		dl_desc.innerText = "How to install on Safari";
 		dl_img.src = "src/images/safari.svg";
 	}
-	else if (navigator.userAgent.indexOf("Chrome") !== -1) {
+	else if (isChrome) {
 		dl_a.href = "https://chrome.google.com/webstore/detail/bonjourr-startpage/dlnejlppicbjfcfcedcflplfjajinajd";
 		dl_desc.innerText = "Get Bonjourr on Chrome";
 		dl_img.src = "src/images/chrome.svg";
