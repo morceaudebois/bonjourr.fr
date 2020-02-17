72cd7fe74ab5234801e03998b3764244a35676d2
Victor Azevedo <34415964+victorazevedo-me@users.noreply.github.com>
2020-02-17 07:51:41 +0100

    Added screenshot slideshow

--- src/script.js
+++ src/script.js
@@ -1,15 +1,24 @@
+
 window.onload = function() {
 
-	//signature aleatoire
+	browserDetection()
+	previewSlideshow(6000) //transition time
+	signature()
+}
+
+
+
+function signature() {
 	const signature = document.getElementById('signature'),
 		victor = "<a href='https://victor-azevedo.me/'>Victor Azevedo</a>",
 		tahoe = "<a href='https://tahoe.be'>Tahoe Beetschen</a>",
 		concat = (Math.random() > .5 ? `${victor} & ${tahoe}` : `${tahoe} & ${victor}`);
 
 	signature.innerHTML = "<p>Made in France with ❤️<br> by " + concat;
-	
+}
 
-	//download for different browsers
+function browserDetection() {
+
 	const dl_a = document.getElementById("mainDownload"),
 		dl_img = document.getElementById("dl_icon"),
 		dl_desc = document.getElementById("dl_desc");
@@ -47,4 +56,28 @@ window.onload = function() {
 		dl_desc.innerText = "Try Bonjourr online";
 		dl_img.style.display = "none";
 	}
+}
+
+function previewSlideshow(time) {
+
+	/*prepare img pour l'overlay
+	cet element n'est pas dans le l'index pour ne pas
+	charger 2 images en meme temps*/
+
+	let imgnode = document.createElement("img");
+	imgnode.id = "img_below";
+	imgnode.setAttribute("alt", "bonjourr screenshot");
+	imgnode.setAttribute("draggable", "false");
+	imgnode.src = "src/images/preview2.jpg";
+
+	document.getElementById("screens").appendChild(imgnode);
+
+	//toggle below opacity (-1s transition)
+	let changed = false;
+	let interval = setInterval(function() {
+
+		document.getElementById('img_below').style.opacity = (changed ? "0" : "1")
+		changed = (changed ? false : true) //toggle
+
+	}, time)
 }
