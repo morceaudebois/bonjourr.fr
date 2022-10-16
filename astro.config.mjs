995429de49a109ee0dd4a170e251b6f331d0e454
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-10-16 11:38:18 +0200

    Removed mdx from integrations for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import mdx from '@astrojs/mdx'
 import svelte from '@astrojs/svelte'
 
 export default defineConfig({
-	integrations: [mdx(), svelte()],
+	integrations: [svelte()],
 })
