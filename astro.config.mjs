2241040bb3dffc778ef9f3a58e4869e1fe929b90
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-10-17 18:19:39 +0200

    Back to static, keeping pnpm on netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
-import netlify from '@astrojs/netlify/edge-functions'
 
 export default defineConfig({
 	integrations: [svelte()],
 	// base: '/astro',
-	output: 'server',
-	adapter: netlify(),
 })
