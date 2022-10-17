93f1ff95657c8a02c708f044fc8d90fb39cb6654
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-10-17 18:12:16 +0200

    Testing Netlify SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
-
-// https://astro.build/config
 import svelte from '@astrojs/svelte'
+import netlify from '@astrojs/netlify/edge-functions'
 
 export default defineConfig({
 	integrations: [svelte()],
 	// base: '/astro',
+	output: 'server',
+	adapter: netlify(),
 })
