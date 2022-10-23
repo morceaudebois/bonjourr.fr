3348b9a8348a4c6652ccc05e874849b5bb064027
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2022-10-23 10:29:19 +0200

    Use "Link" component for internal l10n

--- src/locales/useTranslation.ts
+++ src/locales/useTranslation.ts
@@ -2,18 +2,17 @@ import type { AstroGlobal } from 'astro'
 import FrenchDict from './fr.json'
 
 export default function useTranslation(Astro: AstroGlobal) {
-	function locale() {
+	function rootPathname() {
 		return Astro.url.pathname.substring(1, 3)
 	}
 
-	function isFrench() {
-		// Works because translated version
-		// of website is https://[domain]/fr/[whatever]
-		return locale() === 'fr'
-	}
+	// Works because translated version
+	// of website is https://[domain]/fr/[whatever]
+	const isFrench = rootPathname() === 'fr'
+	const locale = rootPathname() === 'fr' ? '/fr' : ''
 
 	function t(text: string) {
-		if (isFrench() && text in FrenchDict) {
+		if (isFrench && text in FrenchDict) {
 			// text is a key in the translation dictionary
 			return FrenchDict[text as keyof typeof FrenchDict]
 		}
