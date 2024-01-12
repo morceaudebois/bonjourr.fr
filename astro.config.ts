57c323f03d3ff321bc46639871651518ce11651e
Victor Azevedo <victor.azevedo@mail.com>
2024-01-12 17:19:20 +0100

    Added robots.txt and sitemap

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig, squooshImageService } from 'astro/config'
+import sitemap from '@astrojs/sitemap'
+
+// https://docs.astro.build/en/guides/integrations-guide/sitemap/#i18n
+const sitemapOptions = {
+	i18n: {
+		defaultLocale: 'en',
+		locales: {
+			en: 'en',
+			fr: 'fr',
+		},
+	},
+}
 
 export default defineConfig({
+	site: 'https://bonjourr.fr',
+	integrations: [sitemap(sitemapOptions)],
 	image: {
 		service: squooshImageService(),
 	},
