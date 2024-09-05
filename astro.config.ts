2e4311303eb19d76afe394b1cb5a7860bd245b0d
Victor Azevedo <34415964+victrme@users.noreply.github.com>
2024-09-05 12:41:53 +0200

    Double quotes, stronger prettier config

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig, squooshImageService } from 'astro/config'
-import sitemap from '@astrojs/sitemap'
+import { defineConfig, squooshImageService } from "astro/config"
+import sitemap from "@astrojs/sitemap"
 
 // https://docs.astro.build/en/guides/integrations-guide/sitemap/#i18n
 const sitemapOptions = {
 	i18n: {
-		defaultLocale: 'en',
+		defaultLocale: "en",
 		locales: {
-			en: 'en',
-			fr: 'fr',
+			en: "en",
+			fr: "fr",
 		},
 	},
 }
 
 export default defineConfig({
-	site: 'https://bonjourr.fr',
+	site: "https://bonjourr.fr",
 	devToolbar: { enabled: false },
 	integrations: [sitemap(sitemapOptions)],
 	image: {
